f65b76ee90cc6bc30e9c7de9ce3178d9271e8220
myukang <myunghwan0421@gmail.com>
2022-08-02 14:15:41 +0900

    map matrix

--- dfs/mapdef.h
+++ dfs/mapdef.h
@@ -6,21 +6,30 @@
 
 #define NUM_DIRECTIONS 4
 
+//matrix
+/*
+ *    x ->
+ *  y 0 1 2 3 4
+ *  | 1
+ *    2
+ *    3
+ * */
+
 // offset: x, y
 static int DIRECTION_OFFSETS[NUM_DIRECTIONS][2] = {
-	{0, -1},		// 위쪽으로 이동.
-	{1, 0},			// 오른쪽으로 이동.
-	{0, 1},			// 아래쪽으로 이동.
-	{-1, 0}			// 왼쪽으로 이동.
+	{0, -1},		// down
+	{1, 0},			// right
+	{0, 1},			// up
+	{-1, 0}			// left
 };
 
 enum PosStatus { NOT_VISIT = 0, WALL = 1 , VISIT = 2  };
 
 typedef struct MapPositionType
 {
-	int x;				// 현재 위치 x좌표.
-	int y;				// 현재 위치 y좌표.
-	int direction;		// 다음 위치로 이동 방향.
+	int x;				// ???? 위치 x??표.
+	int y;				// ???? 위치 y??표.
+	int direction;		// ??음 위치?? ?絹? ????.
 } MapPosition;
 
 #endif
