e76e48c636a102b9ac9dc905ea0ef11a0c03db3b
Myunghwan Kang <myukang@c10r4s13.42seoul.kr>
2022-08-08 20:39:02 +0900

    MAIN UPDATE

--- queue/linkeddeque/main.c
+++ queue/linkeddeque/main.c
@@ -1,27 +1,92 @@
 #include "linkeddeque.h"
+#include <stdio.h>
 
-void prin:wqt
+void printhead(LinkedDeque *dq)
+{
+	printf("============\n");
+	printf("cur cnt : %d\n", dq->currentElementCount);
+	if (dq->pFrontNode)
+		printf("front : %c\n", dq->pFrontNode->data);
+	if (dq->pRearNode)
+		printf("rear : %c\n", dq->pRearNode->data);
+	printf("============\n");
+}
 
 void test1()
 {
 	LinkedDeque *ld = createLinkedDeque();
-
 	DequeNode tmp;
-	tmp.data = 'A';
+	DequeNode *pop;
 
+	tmp.data = 'A';
+	printf("********insertfront A********\n");
+	insertFrontLD(ld, tmp);
+	printhead(ld);
+
+	tmp.data = 'B';
+	printf("********insertfront B********\n");
+	insertFrontLD(ld, tmp);
+	printhead(ld);
+
+	tmp.data = 'C';
+	printf("********insertrear C********\n");
+	insertRearLD(ld, tmp);
+	printhead(ld);
+
+	printf("********deleteFront********\n");
+	pop = deleteFrontLD(ld);
+	free(pop);
+	printhead(ld);
+
+	printf("********deleteFront********\n");
+	pop = deleteFrontLD(ld);
+	free(pop);
+	printhead(ld);
+
+	printf("********deleteFront********\n");
+	pop = deleteFrontLD(ld);
+	free(pop);
+	printhead(ld);
+
+	deleteLinkedDeque(ld);
 }
 
 void test2()
-{}
+{
+	LinkedDeque *ld = createLinkedDeque();
+	DequeNode *pop;
 
-void test3()
-{}
+	DequeNode tmp;
+	tmp.data = 'A';
+	insertFrontLD(ld, tmp);
+	tmp.data = 'B';
+	insertFrontLD(ld, tmp);
+	tmp.data = 'C';
+	insertFrontLD(ld, tmp);
+	printf("*******initial*******\n");
+	printhead(ld);
 
-void test4()
-{}
+	printf("********deleteRear********\n");
+	pop = deleteRearLD(ld);
+	free(pop);
+	printhead(ld);
 
+	printf("********deleteRear********\n");
+	pop = deleteRearLD(ld);
+	free(pop);
+	printhead(ld);
+	printf("********deleteRear********\n");
+	pop = deleteRearLD(ld);
+	free(pop);
+	printhead(ld);
+	deleteLinkedDeque(ld);
+}
 
 int  main(void)
 {
+	test1();
+	test2();
+	while (1)
+		;
 	return 0;
 }
